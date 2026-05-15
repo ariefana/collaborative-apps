ef181c805f2c21c2c496e655d381a55f20da03ae
ariefana <ariefmudasir79@gmail.com>
2026-05-15 15:02:09 +0700

    mendaftarkan route

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
+use App\Http\Controllers\DocumentController;
 
 Route::get('/', function () {
     return Inertia::render('Welcome', [
@@ -22,6 +23,12 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::get('/dashboard', [DocumentController::class, 'index'])->name('dashboard');
+    Route::post('/documents', [DocumentController::class, 'store'])->name('documents.store');
+    Route::get('/documents/{document:slug}', [DocumentController::class, 'show'])->name('documents.show');
 });
 
 require __DIR__.'/auth.php';
