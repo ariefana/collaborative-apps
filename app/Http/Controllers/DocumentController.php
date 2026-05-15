711347b6f64c3c3cda371dd44a143bdb79b0d9b2
ariefana <ariefmudasir79@gmail.com>
2026-05-15 15:00:25 +0700

    membuat document controller

---
+++ app/Http/Controllers/DocumentController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Document;
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+use Illuminate\Support\Str;
+
+class DocumentController extends Controller
+{
+    // Menampilkan daftar dokumen
+    public function index()
+    {
+        return Inertia::render('Dashboard',[
+            'documents' => auth()->user()->documents()->latest()->get()
+        ]);
+    }
+
+    // Proses membuat dokumen baru
+    public function store(Request $request)
+    {
+        $request->validate(['title' => 'required|string|max:255']);
+
+        $document = auth()->user()->documents()->create([
+            'title' => $request->title,
+            'slug'  => Str::slug($request->title) . '-' . Str::random(5),
+            'content' => '', // awal nya kosong
+        ]);
+
+        return redirect()->route('documents.show', $document->slug);
+    }
+
+    // Halaman editor (Nanti akan dipasang Tiptap di sini)
+    public function show(Document $document)
+    {
+        return Inertia::render('Editor', [
+            'document' => $document
+        ]);
+    }
+}
